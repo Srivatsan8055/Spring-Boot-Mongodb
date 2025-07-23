a6a4b135f884b9a65f3240d9c8278f1f8f576cd9
Srivatsan Sridhar <srivatsansridhar57@gmail.com>
2025-07-24 03:24:53 +0530

    added templates and updated controller

--- src/main/java/net/springboot/srivatsan/controller/ProductController.java
+++ src/main/java/net/springboot/srivatsan/controller/ProductController.java
@@ -3,49 +3,56 @@ package net.springboot.srivatsan.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import net.springboot.srivatsan.model.Product;
 import net.springboot.srivatsan.service.ProductService;
 
-@RestController
+@Controller
+@RequestMapping("/products")
 public class ProductController {
 
-	@Autowired
-	private ProductService productService;
-	
-	@GetMapping("/products")
-	public ResponseEntity<List<Product>> getAllProduct(){
-		return ResponseEntity.ok().body(productService.getAllProduct());
-	}
-	
-	@GetMapping("/products/{id}")
-	public ResponseEntity<Product> getProductById(@PathVariable long id){
-		return ResponseEntity.ok().body(productService.getProductById(id));
-	}
-	
-	@PostMapping("/products")
-	public ResponseEntity<Product> createProduct(@RequestBody Product product){
-		return ResponseEntity.ok().body(this.productService.createProduct(product));
-	}
-	
-	@PutMapping("/products/{id}")
-	public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product){
-		product.setId(id);
-		return ResponseEntity.ok().body(this.productService.updateProduct(product));
-	}
+    @Autowired
+    private ProductService productService;
 
-	@DeleteMapping("/products/{id}")
-	public HttpStatus deleteProduct(@PathVariable long id){
-		this.productService.deleteProduct(id);
-		return HttpStatus.OK;
-	}
+    @GetMapping
+    public String getAllProducts(Model model) {
+        List<Product> list = productService.getAllProduct();
+        model.addAttribute("products", list);
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "add-product";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute Product product) {
+        productService.createProduct(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "edit-product";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable long id, @ModelAttribute Product product) {
+        product.setId(id);
+        productService.updateProduct(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+        return "redirect:/products";
+    }
 }
